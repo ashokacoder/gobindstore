024d2b1c0cc95cd0113eaa88ff068b5a585e3482
ashokacoder <86926629+ashokacoder@users.noreply.github.com>
2021-08-10 22:09:14 +0530

    add file

---
+++ nav.js
@@ -0,0 +1,40 @@
+const toggle = document.querySelector('.toggal');
+const navlinks = document.querySelector('.navlinks');
+const close = document.querySelector('.close');
+
+
+
+toggle.addEventListener('click', ()=>{
+
+    navlinks.classList.toggle('active');
+
+
+});
+
+close.addEventListener('click', ()=>{
+
+    navlinks.classList.toggle('active');
+    close.style.color = 'red';
+
+});
+
+
+
+
+const prodata = document.querySelectorAll(".prodbox2");
+
+     for(i = 0; i < prodata.length; i++){
+   const addli = document.querySelectorAll(".prodbox2")[i];
+    addli.getElementsByTagName("button")[0].innerHTML = `<a href="/gobindstore/contect.html">BUY NOW</a>`;
+
+
+}
+
+
+
+
+
+
+
+
+
